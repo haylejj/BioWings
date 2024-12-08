5c84411f6c3cb127c49821c0654f873fe8411c6a
çağatay cingiz <cagatayberk@windowslive.com>
2024-12-08 11:40:20 +0300

    bff

--- BioWings.Application/obj/Debug/net8.0/BioWings.Application.AssemblyInfo.cs
+++ BioWings.Application/obj/Debug/net8.0/BioWings.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioWings.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0320830750fd84715c2272ac34156c0d815ff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb8f20e0cefa31f8400aec011dbeed64aba392d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioWings.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioWings.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
