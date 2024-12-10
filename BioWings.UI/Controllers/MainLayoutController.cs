bff44730db7932cef8ee7da140f1a1497460328c
çağatay cingiz <cagatayberk@windowslive.com>
2024-12-10 17:48:58 +0300

    main layout

---
+++ BioWings.UI/Controllers/MainLayoutController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BioWings.UI.Controllers;
+public class MainLayoutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
